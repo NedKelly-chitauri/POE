d6043d067ce4bb58775d846421ca6e73c7f59bfd
RC_Student_lab <RC_Student_lab@10.0.0.32>
2024-09-29 15:27:36 +0000

    POE Part one

---
+++ src/main/java/com/mycompany/st10450109/prog5121/poe/ST10450109PROG5121POE.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st10450109.prog5121.poe;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ST10450109PROG5121POE {
+
+    public static void main(String[] args) {
+UserAuthenticationSystem authentic=new UserAuthenticationSystem();
+authentic.registerUser(Scanner scanner);
+    }
+}
+
+
