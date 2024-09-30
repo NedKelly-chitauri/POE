a5a206fb7656a4ca00a74a7b710b66596d32f113
RC_Student_lab <RC_Student_lab@10.0.0.32>
2024-09-30 00:00:30 +0000

    POE Part one: User information class added as a module

--- src/main/java/UserInfo.java
+++ src/main/java/UserInfo.java
@@ -8,5 +8,67 @@
  * @author RC_Student_lab
  */
 public class UserInfo {
-    
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    // Constructor
+    public UserInfo(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and Setters for each field
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // Validation method for username (contains an underscore and is no more than 5 characters long)
+    public static boolean isValidUsername(String username) {
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    // Validation method for password (length, capital letter, number, special character)
+    public static boolean isValidPassword(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+        // Check for at least one capital letter, one number, and one special character
+        if (!password.matches(".*[A-Z].*") || !password.matches(".*\\d.*") || !password.matches(".*[!@#$%^&*()_+].*")) {
+            return false;
+        }
+        return true;
+    }
 }
+
